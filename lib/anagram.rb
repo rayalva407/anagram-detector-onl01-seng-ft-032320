8aa339d34b7e35a9e1f755899d53cda828d17cc6
Ray Alva <rayalva407@gmail.com>
2020-04-04 20:13:35 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,11 +1,13 @@
 class Anagram
+  attr_accessor :word
   
   def initialize(word)
     @word = word
   end
   
   def match(anagrams)
-    
+    anagrams.each do |word|
+      word.split.sort = @word.split.sort
   end
   
 end
